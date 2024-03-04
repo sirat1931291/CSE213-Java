1549f77f781b2dbe96bbdffa796afe382caac3b4
sirat1931291 <128386512+sirat1931291@users.noreply.github.com>
2024-03-04 11:07:58 +0600

    Create FXMLDocumentController.java

---
+++ Spring2024/04-03-2024/FXMLDocumentController.java
@@ -0,0 +1,56 @@
+package mainpkg;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import java.io.BufferedReader;
+
+/**
+ *
+ * @author cis101
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    private String filename = "C:\\Users\\cis101\\Desktop\\CSE213\\Class Code\\04-03-2024\\Java Application\\text.txt";
+    @FXML
+    private TextArea textArea;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void saveFile(ActionEvent event) {
+    }
+
+    @FXML
+    private void loadFile(ActionEvent event) {
+        try{
+            String data = ""; 
+            File file = new File(filename);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = reader.readLine();
+            while (line != null && line.isEmpty()) {
+                data += line + '\n';
+                
+            }
+            textArea.setText(data);
+        }
+        catch (IOException e) {
+            textArea.setText("Shutiye lal korar aage thik kor");
+        }
+    }
+}
